94f8ec028f56d536966728a0b7bc5b373865ea52
Kashi Vishwanath Revanna <kashi.mail@gmail.com>
2024-08-10 11:13:23 -0500

    add run.py

---
+++ run.py
@@ -0,0 +1,37 @@
+import os
+
+from fastapi import FastAPI
+from langserve import add_routes
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_ollama import OllamaLLM
+
+
+llm = OllamaLLM(model="mistral")
+parser = StrOutputParser()
+
+system_template = "Translate the following into {language}:"
+
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+
+chain = prompt_template | llm | parser
+
+app = FastAPI(
+  title="simpleTranslator",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
+
+add_routes(
+    app,
+    chain,
+    path="/chain",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
